bf10ffba16e01eda82a6d25ebc63e86f0634cb36
lfelipeviana <lfelipeviana@gmail.com>
2021-08-14 16:23:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)        
+    server = app.server
 
     components_html = []
     # # luiz viana
